7a2653a0e2fb3f3912a83584951ddafcababb2bf
Darko Stepanovic <darko.stepanovic@computerrock.com>
2017-09-07 12:26:21 +0200

    Fetch todos from firebase on app start

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -90,17 +90,37 @@ describe('Actions', () => {
         var testTodoRef;
 
         beforeEach((done) => {
-            testTodoRef = firebaseRef.child('todos').push();
+            var todosRef = firebaseRef.child('todos');
 
-            testTodoRef.set({
-                text: 'Something to do',
-                completed: false,
-                createdAt: 1234123
-            }).then(() => done());
+            todosRef.remove().then(() => {
+                testTodoRef = firebaseRef.child('todos').push();
+
+                return testTodoRef.set({
+                    text: 'Something to do',
+                    completed: false,
+                    createdAt: 1234123
+                })
+            })
+            .then(() => done())
+            .catch(done);
         });
 
         afterEach((done) => {
             testTodoRef.remove().then(() => done());
+        });
+
+        it('should populate todos dispatch add todo action', (done) => {
+            const store = createMockStore({});
+            const action = actions.startAddTodos();
+
+            store.dispatch(action).then(() => {
+                const mockActions = store.getActions();
+
+                expect(mockActions[0].type).toEqual('ADD_TODOS');
+                expect(mockActions[0].todos.length).toEqual(1);
+                expect(mockActions[0].todos[0].text).toEqual('Something to do');
+                done();
+            }, done);
         });
 
         it('should toggle todo and dispatch update todo action', (done) => {
